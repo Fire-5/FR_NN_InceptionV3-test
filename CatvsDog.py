aac77133e3d8f44e45f098fab7deb2dd32325aca
Fire-5 <alexandr_krasnot@mail.ru>
2022-04-01 18:35:11 +0700

    uodate 1.04.2022

--- CatvsDog.py
+++ CatvsDog.py
@@ -4,11 +4,9 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
-
-
-# TODO: Нужно заставить дома работать на GPU.
-#  Пока что ругается и делает на основном процессоре
 
 
 def scalePicture(img, x, y):
@@ -33,123 +31,118 @@ def transform_gray(image):
     ret, threshold_image = cv2.threshold(gray_image, 127, 255, 0)
     return gray_image
 
-
-def getDataset(path, lenght):
-    # ---> Нобор и создание датасета <---
-    dataset_value = []
-    dataset_key = []
-    os.chdir(path)
-    count = 0
-
-    for dir in os.listdir():
-        os.chdir(dir)
-        for file in os.listdir()[:lenght]:
-            # print('>>>', file)
-            if '.db' == file:
-                continue
-            image = cv2.imread(file)
-            if image is None:
-                continue
-            # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            image_2 = scalePicture(image, 299, 299)
-            dataset_value.append(np.array(image_2).astype('float32'))
-            dataset_key.append(count)
-
-        os.chdir('..')
-        count += 1
-
-    dataset_value = np.asarray(dataset_value)
-    dataset_value = tf.keras.applications.inception_v3.preprocess_input(dataset_value, data_format=None)
-    dataset_key = np.asarray(dataset_key)
-
-    return dataset_value, dataset_key
-
-
+# ---> Нобор и создание датасета <---
+# Полезное чтиво
+# https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/ImageDataGenerator
+# https://gist.github.com/fchollet/0830affa1f7f19fd47b06d4cf89ed44d
 PATH = os.getcwd()
 
-x, y = getDataset(PATH + '/dataset/PetImages/', lenght=1000)
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+train_data_dir = PATH + '/dataset/train'
+validation_data_dir = PATH + '/dataset/validation'
+img_width, img_height = 150, 150
+batch_size = 16
 
-X_train = np.asarray(X_train)
-X_test = np.asarray(X_test)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
+train_datagen = ImageDataGenerator(
+      rescale=1./255, # изменения по размерам изображения
+      rotation_range=40,  # диапазон, в пределах которого можно произвольно поворачивать изображения
+      width_shift_range=0.2,  # это диапазоны (в виде доли от общей ширины или высоты), в пределах которых можно
+      height_shift_range=0.2, # произвольно перемещать изображения по вертикали или горизонтали
+      shear_range=0.2,  # предназначен для случайного применения преобразований сдвига
+      zoom_range=0.2,  # предназначен для случайного масштабирования внутри изображений
+      horizontal_flip=True, # предназначен для случайного переворачивания половины изображений по горизонтали
+      fill_mode='nearest' # заполнения вновь созданных пикселей, которые могут появиться после поворота или сдвига
+)
+test_datagen = ImageDataGenerator(rescale=1./255,)
 
-print('Shape:', X_train.shape, y_train.shape, X_test.shape, y_test.shape, sep='\n[+] > ')
+train_gen = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+test_gen = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
 
 # ---> Создание модели <---
-input_tensor = keras.layers.Input(shape=(299, 299, 3))
+input_tensor = keras.layers.Input(shape=(150, 150, 3))
 base_model = keras.applications.InceptionV3(
     include_top=False,  # включать ли полносвязный слой вверху как последний слой сети
     weights="imagenet",  # предварительное обучение или путь к загружаемому файлу весов
     input_tensor=input_tensor,  # необязательный тензор для использования в качестве входного изображения для модели
-    input_shape=None,  # необязательный кортеж формы входного изображения
+    input_shape=(150, 150, 3),  # необязательный кортеж формы входного изображения
     pooling=None,  # дополнительный режим объединения для извлечения признаков (avg,max)
     classes=2,  # необязательное количество классов для классификации изображений
     classifier_activation="softmax",  # Функция активации для использования на «верхнем» слое
 )
+# ---> Делаем все слои необучаемыми <---
+for layer in base_model.layers:
+    layer.trainable = False
 
 # ---> Создание новой модели сверху. <---
-x = base_model.output
-x = keras.layers.GlobalAveragePooling2D()(x)
+# x = base_model.output
+# x = keras.layers.GlobalAveragePooling2D()(x)
+# x = keras.layers.Dense(1024, activation='relu')(x)
+# predictions = keras.layers.Dense(1, activation='softmax')(x)
+
+x = keras.layers.Flatten()(base_model.output)
 x = keras.layers.Dense(1024, activation='relu')(x)
-predictions = keras.layers.Dense(1, activation='softmax')(x)
+x = keras.layers.Dropout(0.2)(x)
+predictions = keras.layers.Dense(1, activation='sigmoid')(x)
 
 model = keras.models.Model(inputs=base_model.input,
                            outputs=predictions)
 
-for layer in base_model.layers:
-    layer.trainable = False
-
-
-# model = base_model
-model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy')
+model.compile(optimizer=RMSprop(learning_rate=0.0001),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
 
 # ---> Обучение модели <---
-history = model.fit(X_train,
-                    y_train,
-                    epochs=3,
-                    batch_size=50,
-                    validation_data=(X_test, y_test))
+history = model.fit(
+    train_gen,
+    epochs=15,
+    validation_data=test_gen
+)
 
-# ---> Тонкая настройка модели <---
-for i, layer in enumerate(base_model.layers):
-   print(i, layer.name)
-
-for layer in model.layers[:249]:
-   layer.trainable = False
-for layer in model.layers[249:]:
-   layer.trainable = True
-
-model.compile(optimizer=keras.optimizers.SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
-
-history = model.fit(X_train,
-                    y_train,
-                    epochs=3,
-                    batch_size=50,
-                    validation_data=(X_test, y_test))
-
-model.save(PATH + '/my_model.h5')
+# history.save(PATH + '/my_model.h5')
+# history.save_weights(PATH + '/first_try.h5')
 
 # history = model.load(PATH + '/my_model.h5')
 
-model.summary()
+# model.summary()
 print(history.history.keys())
-# summarize history for accuracy (jupyter only)
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
-plt.style.use('_mpl-gallery')
+epochs = range(len(acc))
 
-fig, ax = plt.subplots(2, 1)
+plt.plot(epochs, acc, 'bo', label='training accuracy')
+plt.plot(epochs, val_acc, 'b', label='validation accuracy')
+plt.legend()
+plt.figure()
 
-# ax[0].plot(history.history['binary_accuracy'], history.history['val_binary_accuracy'])  # RAISE ERROR
-ax[0].plot(history.history['loss'], history.history['val_loss'])  # RAISE ERROR
+plt.plot(epochs, loss, 'bo', label='training loss')
+plt.plot(epochs, val_loss, 'b', label='validation loss')
+plt.legend()
 
-ax[0].set_ylabel('accuracy')
-ax[0].set_xlabel('epoch')
-ax[1].set_ylabel('loss')
-ax[0].legend(['train', 'test'], loc='upper left')
+# plt.style.use('_mpl-gallery')
+# fig, ax = plt.subplots(2, 1)
+#
+# ax[0].plot(history.history['acc'], history.history['val_acc'])  # RAISE ERROR
+# ax[1].plot(history.history['loss'], history.history['val_loss'])  # RAISE ERROR
+#
+# ax[0].set_ylabel('acc')
+# ax[0].set_xlabel('epoch')
+# ax[1].set_ylabel('loss')
+# ax[0].legend(['train', 'test'], loc='upper left')
+#
+# fig.tight_layout()
 
-fig.tight_layout()
 plt.show()
 plt.close()
